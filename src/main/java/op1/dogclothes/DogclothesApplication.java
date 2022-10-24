65bc03aeac611254bad06286b1ebdf3090b92bea
AaltonenSan <santtu.36@gmail.com>
2022-10-24 10:24:05 +0300

    first commit

---
+++ src/main/java/op1/dogclothes/DogclothesApplication.java
@@ -0,0 +1,13 @@
+package op1.dogclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogclothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogclothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/op1/dogclothes/DogclothesApplicationTests.java
@@ -0,0 +1,13 @@
+package op1.dogclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogclothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
