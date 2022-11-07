5d4f84fd074caef176c62925214fbaf3756e672d
JyriLampio <jyri.lampio@gmail.com>
2022-11-07 10:48:51 +0200

    Add Database testcases

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import fi.vaatteet.demo.DemoApplication;
+import fi.vaatteet.demo.web.ManufacturerController;
+import fi.vaatteet.demo.web.ProductController;
+
+@ContextConfiguration(classes = DemoApplication.class)
 @SpringBootTest
 class DemoApplicationTests {
+	
+	@Autowired
+	ManufacturerController manController;
+	@Autowired
+	ProductController prodController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(manController).isNotNull();
+		assertThat(prodController).isNotNull();
+		
 	}
 
 }
---
+++ src/test/java/com/example/demo/ProductRepoTests.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import fi.vaatteet.demo.DemoApplication;
+import fi.vaatteet.demo.domain.Manufacturer;
+import fi.vaatteet.demo.domain.Product;
+import fi.vaatteet.demo.domain.ProductRepo;
+
+@ContextConfiguration(classes = DemoApplication.class)
+@DataJpaTest
+class ProductRepoTests {
+	
+	@Autowired
+	ProductRepo productRepo;
+	
+	@Test
+	public void FindClothesname() {
+		
+		Product product = productRepo.findById((long) 3).get();
+		assertEquals(product.getName(), "JoustavaMeno");
+	}
+	
+	@Test
+	public void FindClothesManufacturer() {
+		
+		Manufacturer manuF = productRepo.findById((long) 3).get().getManufacturer();
+		//String produsct = productRepo.findById((long) 3).get().getName();
+		assertEquals(manuF.getName(), "M&M");
+	}
+
+
+}
