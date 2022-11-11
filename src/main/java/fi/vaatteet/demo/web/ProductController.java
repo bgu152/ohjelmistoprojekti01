f19600e0275465e491d60c19b5f16bba4ff8ca83
AaltonenSan <santtu.36@gmail.com>
2022-11-11 10:44:48 +0200

    /api-hakemisto

--- src/main/java/fi/vaatteet/demo/web/ProductController.java
+++ src/main/java/fi/vaatteet/demo/web/ProductController.java
@@ -3,13 +3,12 @@ package fi.vaatteet.demo.web;
 import java.util.List;
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +23,7 @@ import fi.vaatteet.demo.domain.ManufacturerRepo;
 import fi.vaatteet.demo.domain.Product;
 import fi.vaatteet.demo.domain.ProductRepo;
 
+@CrossOrigin
 @Controller
 public class ProductController {
 	@Autowired
