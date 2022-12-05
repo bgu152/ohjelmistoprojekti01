5bad59fd0fb190c9477bb2a64298b822b145504e
bgu152 <erikrammschmidt@gmail.com>
2022-12-05 10:40:25 +0200

    MariaDB käyttöön väliaikaisesti jotta voin tehdä uuden buildin

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 SET FOREIGN_KEY_CHECKS= 0;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS manufacturer;
+DROP TABLE IF EXISTS users;
 CREATE TABLE manufacturer
 (
    id INT (11) NOT NULL AUTO_INCREMENT,
@@ -15,5 +16,13 @@ CREATE TABLE product
    price FLOAT,
    manufacturer_id INT (11),
    PRIMARY KEY (id),
-   FOREIGN KEY (manufacturer_id) REFERENCES manufacturer(id)
+   FOREIGN KEY (manufacturer_id) REFERENCES manufacturer (id)
+);
+CREATE TABLE users
+(
+   id INT (11) NOT NULL AUTO_INCREMENT,
+   username VARCHAR (255) NOT NULL,
+   password VARCHAR (255) NOT NULL,
+   role VARCHAR (255) NOT NULL,
+   PRIMARY KEY (id)
 );
